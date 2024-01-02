032105f10a198a7c2031d8a2f4d208155e0aa3aa
Giovani Mombelli Mussio <giomussio@gmail.com>
2024-01-02 17:47:48 -0300

    Adding test to validate the duplicated behaviour

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -2,13 +2,26 @@ const { test } = require("../support");
 const data = require("../support/fixtures/movies.json");
 const { executeSQL } = require("../support/database");
 
+test.beforeAll(async () => {
+  await executeSQL(`DELETE FROM movies;`);
+});
 test("deve cadastrar um novo filme", async ({ page }) => {
   const movie = data.create;
-  await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}';`);
 
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.create(movie);
   await page.toast.containText("Cadastro realizado com sucesso!");
+});
+test("nao deve cadastrar quando o titulo eh duplicado", async ({ page }) => {
+  const movie = data.duplicate;
+
+  await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
+  await page.movies.create(movie);
+  await page.toast.containText("Cadastro realizado com sucesso!");
+  await page.movies.create(movie);
+  await page.toast.containText(
+    "Oops!Este conteúdo já encontra-se cadastrado no catálogo"
+  );
 });
 test("nao deve cadastrar quando os campos obrigatorios nao sao preenchidos", async ({
   page,
