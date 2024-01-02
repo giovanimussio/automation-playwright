8034cc7f6284cdeb3f12d4f168035be65899bd29
Giovani Mombelli Mussio <giomussio@gmail.com>
2024-01-02 18:56:50 -0300

    create a new function to getCompanyIdByName and add to context the setToken function

--- tests/support/api/index.js
+++ tests/support/api/index.js
@@ -7,7 +7,6 @@ export class Api {
   }
 
   async setToken() {
-    await this.setToken();
     const response = await this.request.post("http://localhost:3333/sessions", {
       data: {
         email: "admin@zombieplus.com",
@@ -19,7 +18,22 @@ export class Api {
     const body = JSON.parse(await response.text());
     this.token = "Bearer " + body.token;
   }
+  async getCompanyIdByName(companyName) {
+    const response = await this.request.get("http://localhost:3333/companies", {
+      headers: {
+        Authorization: this.token,
+      },
+      params: {
+        name: companyName,
+      },
+    });
+    expect(response.ok()).toBeTruthy();
+    const body = JSON.parse(await response.text());
+    return body.data[0].id;
+  }
   async postMovie(movie) {
+    const companyId = await this.getCompanyIdByName(movie.company);
+
     const response = await this.request.post("http://localhost:3333/movies", {
       headers: {
         Authorization: this.token,
@@ -29,7 +43,7 @@ export class Api {
       multipart: {
         title: movie.title,
         overview: movie.overview,
-        company_id: "31f9d1f8-0f26-42b2-b369-235980eb08bb",
+        company_id: companyId,
         release_year: movie.release_year,
         featured: movie.featured,
       },
